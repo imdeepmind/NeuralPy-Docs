78a00bd4928c185074b6a3e3f619a44169cd0e2f
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-05-13 00:45:48 +0530

    added header and nav links

--- src/App.js
+++ src/App.js
@@ -2,7 +2,43 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-     
+      <div class="header">
+        <img src={require("./assets/logo.png")} alt="neuralpy logo" />
+        <p>A <strong>Keras</strong> like <strong>deep learning</strong> framework work on top of <strong>PyTorch</strong></p>
+      </div>
+      <div class="row">
+        <div class="col-12 col-md-3">
+        <nav>
+        <ul>
+          <li>
+            <a href="#">Introduction</a>
+            <ul>
+              <li>
+                <a href="#">Introduction</a>
+                <ul>
+                  <li>
+                    <a href="#">Introduction</a>
+                  </li>
+                  <li>
+                    <a href="#">Introduction</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Introduction</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">Introduction</a>
+          </li>
+        </ul>
+      </nav>
+        </div>
+        <div class="col-12 col-md-9">
+          <p>Context goes here</p>
+        </div>
+      </div>
     </div>
   );
 }
