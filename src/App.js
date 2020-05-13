787ddb6da6d38378f4c8224eba422bc9c6cc174a
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-05-13 23:51:01 +0530

    added code for building the links

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
 import React from 'react';
 
 import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+
+import { navLinks } from "./data";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+
       <div class="row">
+
         <div class="col-12 col-md-3">
-          <nav>
-            <ul>
-              <li>
-                <a href="#">Introduction</a>
-                <ul>
-                  <li>
-                    <a href="#">Introduction</a>
-                    <ul>
-                      <li>
-                        <a href="#">Introduction</a>
-                      </li>
-                      <li>
-                        <a href="#">Introduction</a>
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <a href="#">Introduction</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">Introduction</a>
-              </li>
-            </ul>
-          </nav>
+          <Navigation navLinks={navLinks} />
         </div>
+
         <div class="col-12 col-md-9">
           <p>Context goes here</p>
         </div>
+        
       </div>
     </div>
   );
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const buildLink = links => {
+  return (
+    <ul>
+      {links.map(val => {
+        return (
+          <li key={val.link}>
+            <a href={val.link}>{val.title}</a>
+            {val.urls && val.urls.length > 0 && buildLink(val.urls)}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+const Navigation = ({ navLinks }) => {
+  return (
+    <nav>
+      {buildLink(navLinks)}
+    </nav>
+  )
+}
+
+export default Navigation;
