5b59a1f7ded116279e56bea339b667de901f2b87
imdeepmind <abhishekchatterjeejit@gmail.com>
2020-05-17 01:47:47 +0530

    added search functionalist

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const buildLink = links => {
   return (
@@ -16,9 +16,49 @@ const buildLink = links => {
 }
 
 const Navigation = ({ navLinks }) => {
+  const [key, setKey] = useState("");
+  const [links, setLinks] = useState([]);
+
+  const searchList = (list, key) => {
+    let searchedResults = [];
+
+    list.forEach(item => {
+      const title = item.title.toLowerCase();
+
+      const result = title.search(key);
+
+      result >= 0 && searchedResults.push(item);
+
+      if (item.urls && result < 0) {
+        const childResults = searchList(item.urls, key);
+        
+        searchedResults = childResults.length > 0 ? searchedResults.concat(childResults) : searchedResults;
+      }
+    });
+
+    return searchedResults;
+  }
+
+  const handleSearch = (e) => {
+    const value = e.target.value;
+    setKey(value);
+
+    if (value && value.length > 2) {
+      const searchedResults = searchList(navLinks, value);
+
+      setLinks(searchedResults);
+    } else {
+      setLinks(navLinks);
+    }
+  }
+
+  useEffect(() => {
+    setLinks(navLinks)
+  }, [navLinks]);
   return (
     <nav className="navigation">
-      {buildLink(navLinks)}
+      <input type="search" className="nav-search" placeholder="Search the documentation" value={key} onChange={handleSearch} />
+      {buildLink(links)}
     </nav>
   )
 }
