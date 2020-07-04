a723b3fc6c87d73260b9d0b785aca1cdef7fdde9
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-07-04 14:25:00 +0530

    router configured

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./Container/Home";
 import Installation from "./Container/Installation";
 import Error from "./Container/Error";
 
+import NavBar from "./Components/NavBar";
+
 const App = () => {
   return (
     <div className="App">
-     <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/installation" component={Installation} />
-        <Route path="*" component={Error} />
-      </Switch>
-     </BrowserRouter>
+      <NavBar />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/installation" component={Installation} />
+          <Route path="*" component={Error} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
