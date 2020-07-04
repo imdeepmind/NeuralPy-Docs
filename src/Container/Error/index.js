6635257f4693afab45c11d71f400d7899e876f15
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-07-04 14:04:23 +0530

    configured react router dom

---
+++ src/Container/Error/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Error = props => {
+    return (
+        <h1>Error Page</h1>
+    );
+}
+
+export default Error;
---
+++ src/Container/Installation/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Installation = props => {
+    return (
+        <h1>Installation Page</h1>
+    );
+}
+
+export default Installation;
