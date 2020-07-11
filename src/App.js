6f2d4ba5b1ff84b14b79ceed0920c679e66ea8e7
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-07-11 23:43:37 +0530

    restructured the code

--- src/App.js
+++ src/App.js
@@ -1,80 +1,104 @@
-import React, { useState, useEffect, version } from 'react';
-import Axios from "axios";
+import React from "react";
 
-import Home from "./Container/Home";
+import Header from "./Components/Header";
+import Navigation from "./Components/Navigation";
+import Contents from "./Components/Contents";
 
-const App = () => {
-  const [contents, setContent] = useState(null);
-  const [docs, setDocs] = useState(null);
-  const [docVersion, setDocVersion] = useState([{"label": "Latest", "value": "master"}]);
-  const [selectedVersion, setSelectedVersion] = useState(null);
-
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  const loadVersions = async () => {
-    try {
-      const versionURL = "https://raw.githubusercontent.com/imdeepmind/NeuralPy/master/docs/versions.json";
-      const versions = await Axios.get(versionURL);
-
-      if (!versions && version.data) {
-        throw new Error("Not able to load the versions list");
-      }
-
-      setDocVersion(versions.data);
-      loadContent(versions.data);
-    } catch (error) {
-      setError(error);
-    }
-  }
-
-  const loadContent = async (versions) => {
-    setLoading(true);
-    try {
-      const selectedDoc = selectedVersion ? selectedVersion.value : versions[0].value;
-      const contentUrl = `https://raw.githubusercontent.com/imdeepmind/NeuralPy/${selectedDoc}/docs/contents.json`;
-      const docUrl = `https://raw.githubusercontent.com/imdeepmind/NeuralPy/${selectedDoc}/docs/DOCS.md`;
-      
-      const content = await Axios.get(contentUrl);
-      const docs = await Axios.get(docUrl);
-
-      if (!(content && content.data)) {
-        throw new Error("Not able to load the contents catalog");
-      }
-    
-      if (!(docs && docs.data)) {
-        throw new Error("Not able to load the documentation markdown files");
-      }
-
-      setContent(content.data);
-      setDocs(docs.data);
-      setError(null);
-    } catch (error) {
-      setError(error);
-    }
-    setLoading(false);
-  }
-
-  const changeVersion = (selectedOption) => {
-    setSelectedVersion(selectedOption);
-  }
-
-  useEffect(() => {
-    loadVersions();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedVersion]);
-
+const NeuralPy = () => {
   return (
-    <Home
-      contents={contents || {}}
-      docs={docs}
-      loading={loading}
-      error={error}
-      versions={docVersion}
-      selectedVersion={selectedVersion}
-      changeVersion={changeVersion}
-    />
+    <div className="container">
+      <Header />
+      <div className="row">
+        <div className="col-12 col-md-4 col-lg-3">
+          <Navigation />
+        </div>
+        <div className="col-12 col-md-8 col-lg-9">
+          <Contents />
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
-export default App;
+
+
+// const App = () => {
+//   const [contents, setContent] = useState(null);
+//   const [docs, setDocs] = useState(null);
+//   const [docVersion, setDocVersion] = useState([
+//     { label: "Latest", value: "master" },
+//   ]);
+//   const [selectedVersion, setSelectedVersion] = useState(null);
+
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   const loadVersions = async () => {
+//     try {
+//       const versionURL =
+//         "https://raw.githubusercontent.com/imdeepmind/NeuralPy/master/docs/versions.json";
+//       const versions = await Axios.get(versionURL);
+
+//       if (!versions && version.data) {
+//         throw new Error("Not able to load the versions list");
+//       }
+
+//       setDocVersion(versions.data);
+//       loadContent(versions.data);
+//     } catch (error) {
+//       setError(error);
+//     }
+//   };
+
+//   const loadContent = async (versions) => {
+//     setLoading(true);
+//     try {
+//       const selectedDoc = selectedVersion
+//         ? selectedVersion.value
+//         : versions[0].value;
+//       const contentUrl = `https://raw.githubusercontent.com/imdeepmind/NeuralPy/${selectedDoc}/docs/contents.json`;
+//       const docUrl = `https://raw.githubusercontent.com/imdeepmind/NeuralPy/${selectedDoc}/docs/DOCS.md`;
+
+//       const content = await Axios.get(contentUrl);
+//       const docs = await Axios.get(docUrl);
+
+//       if (!(content && content.data)) {
+//         throw new Error("Not able to load the contents catalog");
+//       }
+
+//       if (!(docs && docs.data)) {
+//         throw new Error("Not able to load the documentation markdown files");
+//       }
+
+//       setContent(content.data);
+//       setDocs(docs.data);
+//       setError(null);
+//     } catch (error) {
+//       setError(error);
+//     }
+//     setLoading(false);
+//   };
+
+//   const changeVersion = (selectedOption) => {
+//     setSelectedVersion(selectedOption);
+//   };
+
+//   useEffect(() => {
+//     loadVersions();
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [selectedVersion]);
+
+//   return (
+//     <Home
+//       contents={contents || {}}
+//       docs={docs}
+//       loading={loading}
+//       error={error}
+//       versions={docVersion}
+//       selectedVersion={selectedVersion}
+//       changeVersion={changeVersion}
+//     />
+//   );
+// };
+
+export default NeuralPy;
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,15 @@
-import React from "react"
+import React from "react";
 
 const Header = () => {
   return (
     <div className="header">
       <img src={require("../../Assets/logo.png")} alt="neuralpy logo" />
-      <p>A <strong>Keras</strong> like <strong>deep learning</strong> library that works on top of <strong>PyTorch</strong></p>
+      <p>
+        A <strong>Keras</strong> like <strong>deep learning</strong> library
+        that works on top of <strong>PyTorch</strong>
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
